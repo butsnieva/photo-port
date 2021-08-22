87f53eac9c439e446fd788e8bfcf657fd15c20cd
Kateryna B <butsneva.ekaterina@gmail.com>
2021-08-22 09:24:39 -0500

    final

---
+++ src/components/Modal/__tests__/index.test.js
@@ -0,0 +1,7 @@
+// import React from 'react'
+// import { cleanup, render } from '@testing-library/react'
+// import '@testing-library/jest-dom/extend-expect'
+// import Modal from '..'
+
+// afterEach(cleanup)
+
